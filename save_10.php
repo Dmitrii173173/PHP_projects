fd09e1f977d8f451577bda6fe62bfec862aeabca
DuMing <dmitrii.perfilev2020@gmail.com>
2023-08-21 00:00:06 +0800

    task10

---
+++ save_10.php
@@ -0,0 +1,10 @@
+<?php
+
+$text = $_POST['text'];
+$pdo = new PDO("mysql:host=localhost;dbname=newdb;", "root", "");
+$sql = "INSERT INTO my_table (text) VALUES (:text)";
+$statement = $pdo -> prepare($sql);
+$statement -> execute(['text' => $text]);
+header("Location: /task/task_10.php")
+?>
+
